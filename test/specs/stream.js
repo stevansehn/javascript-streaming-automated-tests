d2490df093e5d4c5e1137428de0abe4dc2bffb22
Stevan Sehn <stevansehn2015@gmail.com>
2021-01-18 10:36:24 -0300

    corrige https

--- test/specs/stream.js
+++ test/specs/stream.js
@@ -1,14 +1,14 @@
 describe('stream test', () => {
     
     it('abre a página inicial', () => {
-        browser.url('https://localhost:3000/')
+        browser.url('http://localhost:3000/')
     })
 
-    it('demonstrate the alertText command', function () {
-    let alert = browser.alertText();
-    expect(alert).toEqual('Enter room name:');
-    // ...
-    });
+    // it('demonstrate the alertText command', function () {
+    // let alert = browser.alertText();
+    // expect(alert).toEqual('Enter room name:');
+    // // ...
+    // });
 
     it('should save a screenshot of the browser view', function () {
         browser.saveScreenshot('./screenshot.png');
